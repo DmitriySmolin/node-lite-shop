1f93417d6d1b07f9121c69a2fe7d8dde79435170
Dmitriy Smolin <45199267+DmitriySmolin@users.noreply.github.com>
2020-10-15 20:10:46 +0400

    feat: add 07 Node.js и MySQL. Подключаемся и получаем список товаров. Промисы

---
+++ 07 Node.js и MySQL. Подключаемся и получаем список товаров. Промисы/app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+
+// public - папка где хранится статика
+app.use(express.static('public'));
+
+// задаем шаблонизатор pug
+app.set('view engine', 'pug');
+
+let con = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'lite_shop',
+});
+
+// con.connect((err) => {
+//   if (err) throw err;
+//   console.log('Connected');
+// });
+
+app.listen(3000, () => {
+  console.log('/');
+  console.log('node expres work on 3000');
+});
+
+app.get('/', (req, res) => {
+  con.query('SELECT * FROM goods', (err, result) => {
+    if (err) throw err;
+    // console.log(result);
+
+    let goods = {};
+    for (let i = 0; i < result.length; i++) {
+      goods[result[i]['id']] = result[i];
+    }
+
+    // console.log(goods);
+    //console.log(JSON.parse(JSON.stringify(goods)));
+
+    res.render('main', {
+      foo: 'hello',
+      bar: 7,
+      goods: JSON.parse(JSON.stringify(goods)),
+    });
+  });
+});
